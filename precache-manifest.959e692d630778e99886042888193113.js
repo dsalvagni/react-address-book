b037a6664aaad7e8b288f3e7b4a2213f2fb7909b
Daniel Salvagni <danielsalvagni@gmail.com>
2019-04-12 15:55:50 +0200

    Updates GH Pages

---
+++ precache-manifest.959e692d630778e99886042888193113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "570b5a36956d4ad4bfe4",
+    "url": "/react-address-book/static/css/main.7548c6cc.chunk.css"
+  },
+  {
+    "revision": "570b5a36956d4ad4bfe4",
+    "url": "/react-address-book/static/js/main.45e5096a.chunk.js"
+  },
+  {
+    "revision": "76d32c16cc0834134ea6",
+    "url": "/react-address-book/static/js/runtime~main.fcf8e729.js"
+  },
+  {
+    "revision": "e39c9a58221214ed2799",
+    "url": "/react-address-book/static/css/2.23f177a4.chunk.css"
+  },
+  {
+    "revision": "e39c9a58221214ed2799",
+    "url": "/react-address-book/static/js/2.8acb0da8.chunk.js"
+  },
+  {
+    "revision": "cc9ce577a6fffc3571963ca8324279ea",
+    "url": "/react-address-book/index.html"
+  }
+];
